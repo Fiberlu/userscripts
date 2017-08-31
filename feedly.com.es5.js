d6aeceeb227a06e41f97557f5983afa235093847
KKDCAI <421685785@qq.com>
2017-08-31 17:13:16 +0800

    es5

---
+++ feedly.com.es5.js
@@ -0,0 +1,36 @@
+﻿// ==UserScript==
+// @name         feedly.com
+// @namespace    https://github.com/Fiberlu/userscripts
+// @version      0.1
+// @author       映阶碧草
+// @match        https://feedly.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var feelyTabsArea = document;//.querySelector('#feedlyTabs')
+    feelyTabsArea.addEventListener('click', collapseAllSiblingCategories);
+
+    function collapseAllSiblingCategories(event) {
+        let clickedCategory;
+        if (event.target.matches('[id^="user/"][id$=tab_label]')) {
+            event.stopPropagation();
+            clickedCategory = event.target.parentNode.querySelector('.handle');
+            clickedCategory.click();
+            return;
+        }
+        if (event.target.matches('.handle')) {
+            clickedCategory = event.target;
+        } else {
+            return;
+        }
+
+        const categories = feelyTabsArea.querySelectorAll('.handle');
+        for (var category of categories) {
+            if (category == clickedCategory || !category.classList.contains('expanded')) {
+                continue;
+            }
+            category.click();
+        }
+    }
+})
